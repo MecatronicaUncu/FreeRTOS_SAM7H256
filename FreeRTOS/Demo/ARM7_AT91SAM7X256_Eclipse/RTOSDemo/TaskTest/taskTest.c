f670af320dfbf33200281b2c0662b9f8c026a174
Fernando Cladera <fcladera@fcladera.com>
2014-05-20 20:49:59 -0300

    Traducido taskTest.c al español

--- FreeRTOS/Demo/ARM7_AT91SAM7X256_Eclipse/RTOSDemo/TaskTest/taskTest.c
+++ FreeRTOS/Demo/ARM7_AT91SAM7X256_Eclipse/RTOSDemo/TaskTest/taskTest.c
@@ -1,72 +1,62 @@
 #include <stdlib.h>
-
-/* Scheduler include files. */
+/* Includes del Scheduler */
 #include "FreeRTOS.h"
 #include "task.h"
-
-/* Demo program include files. */
+/* Include de taskTest */
 #include "taskTest.h"
 
-#define testSTACK_SIZE		configMINIMAL_STACK_SIZE
+/* Tamaño de pila */
+#define testSTACK_SIZE	configMINIMAL_STACK_SIZE
 
-/* Variable used by the created tasks to calculate the LED number to use, and
-the rate at which they should flash the LED. */
+/* Contador utilizado para identificar las tareas */
 static volatile unsigned taskCounter   = 0;
+
+/* Contador, definido en taskTest.h*/
 static magicStruct myStruct;
 
-/* The task that is created three times. */
+/* Port de la tarea con el macro para la arquitectura */
 static portTASK_FUNCTION_PROTO( vTaskTest, pvParameters );
 
-/*-----------------------------------------------------------*/
-
-void vStartTaskTest( unsigned portBASE_TYPE uxPriority )
-{
-
+/* Función para crear las tareas, en un bucle */
+void vStartTaskTest( unsigned portBASE_TYPE uxPriority ){
+	/* Creamos 8 tareas TaskTest iguales */
 	int i;
-	for(i = 0; i < 7; ++i )
-	{
-		/* Spawn the task. */
+	for(i = 0; i < 7; ++i ){
 		xTaskCreate( vTaskTest, "Tasksx", testSTACK_SIZE, NULL, uxPriority, ( TaskHandle_t * ) NULL );
 	}
 }
-/*-----------------------------------------------------------*/
 
-static portTASK_FUNCTION( vTaskTest, pvParameters )
-{
+/* Definición de la tarea TaskTest */
+static portTASK_FUNCTION( vTaskTest, pvParameters ){
 	TickType_t taskRate, xLastFlashTime;
 	unsigned portBASE_TYPE taskID;
 
-	/* The parameters are not used. */
+	/* Evito errores del compilador */
 	( void ) pvParameters;
 
-	/* Calculate the LED and flash rate. */
+	/* Identificamos cada tarea con un taskID único */
 	portENTER_CRITICAL();
 	{
-		/* See which of the eight LED's we should use. */
 		taskID = taskCounter;
-
-		/* Update so the next task uses the next LED. */
 		taskCounter++;
 	}
 	portEXIT_CRITICAL();
 
-	taskRate = 100 + ( 100 * ( TickType_t ) taskCounter );
+	/* Calculamos el periodo de la tarea utilizando taskID */
+	taskRate = 100 + ( 100 * ( TickType_t ) taskID );
 	taskRate /= portTICK_PERIOD_MS;
 
-	/* We will turn the LED on and off again in the delay period, so each
-	delay is only half the total period. */
-	taskRate /= ( TickType_t ) 2;
-
-	/* We need to initialise xLastFlashTime prior to the first call to
-	vTaskDelayUntil(). */
+	/* Inicialización de xLastFlashTime */
 	xLastFlashTime = xTaskGetTickCount();
 
-	for(;;)
-	{
-		/* Delay for half the flash period then turn the LED on. */
+	for(;;){
+		/* Programa principal:
+		 * La tarea se "duerme" taskRate ms
+		 * Luego, incrementa el contador en myStruct,
+		 * indicando su taskID
+		 */
 		vTaskDelayUntil( &xLastFlashTime, taskRate );
 		myStruct.counter++;
 		myStruct.lastModified = taskID;
 	}
-} /*lint !e715 !e818 !e830 Function definition must be standard for task creation. */
-
+}
